83cc9280704203627dd14e7b75f41977572a5085
Jeff Chen <cytastorm@gmail.com>
2022-09-20 23:04:30 -0400

    test file from home

--- 03_py/test.py
+++ 03_py/test.py
@@ -1,1 +1,69 @@
 ## i like eating eggs
+""" this is test of vim
+:w to save
+:q to quit (no save)
+:wq to save and quit
+keybinds:
+i for insert mode
+esc for normal mode
+h moves cursor left
+l moves cursor right
+j moves cursor down
+k moves cursor up
+
+w for start of next word
+e for end of current word
+b for start of previous word
+
+numbers can be combined with commands to execute them [number] amount of times
+3b for 3 b's, 9l for 9 l's, etc
+numbers can also be used to insert chains of characters
+9i test esc
+testtesttesttesttesttesttesttesttest
+
+f or F to find navigate to next character
+combine with number to find [number]th instance of the character 
+3fo finds third from cursor instance of o
+
+( { [ % to jump to matching paren or bracket
+
+0 to move to beginning of line
+$ to move to end of line
+
+* to find next instance of word under cursor
+# to find previous instance of word under cursor
+
+gg to move to beginning of file
+G to move to end of file
+[number]G to move to line [number]
+
+/ to search for text
+n to move to next occurence
+N to move to previous occurence
+
+o to make a newline below current line 
+poopity scoop
+poopity scoop
+O to make a newline above current line
+
+x removes character under cursor
+X removes character left of cursor
+the quick brown fox jumps over he lzy dog
+
+r replaces character under cursor with next input
+re replace y in my with e for me
+replace me
+
+d combined with number and w b e to cut words and paste to clipboard, p to paste
+d2b deletes 2 words behind cursor
+d2e deletes 2 words in front of cursor
+the quick brown fox jumps over the  
+lazy dog
+
+the quick brown fox  the lazy dog
+jumps over
+
+. repeats previous command (includes edits made in insert mode if command put you in insert mode) 
+o (newline)
+
+""

b2ccc29a55f37ea51da65f293a5dc31ac937dcd6
Jeff <jeff@JeffHome.lan>
2022-09-21 22:46:59 -0400

    added no char code and descriptions

--- 03_py/warmup1/code.py
+++ 03_py/warmup1/code.py
@@ -126,6 +126,19 @@ missing_char('kitten', 1) → 'ktten'
 missing_char('kitten', 0) → 'itten'
 missing_char('kitten', 4) → 'kittn'
 """
+def missing_char(str, n):
+  string = str[0:n]
+  return string + str[n+1:len(str)]
+
+##
+
+"""
+Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+
+missing_char('kitten', 1) → 'ktten'
+missing_char('kitten', 0) → 'itten'
+missing_char('kitten', 4) → 'kittn'
+"""
 def front_back(str):
   if len(str) < 2:
     return str

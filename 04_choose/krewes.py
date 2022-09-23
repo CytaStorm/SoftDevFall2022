b6b5f9a3870113a7f321e5f45f0f3b6b803a2cd5
Jeff Chen <cytastorm@gmail.com>
2022-09-23 09:10:17 -0400

    finished picking random from krewes from notes and code

---
+++ 04_choose/krewes.py
@@ -0,0 +1,33 @@
+"""
+JFCCC: Fang Chen, Jeff Chen
+Soft Dev, pd 2
+
+DISCO:
+Access dict items w/ dictName[key] or dictName.get(key())
+
+QCC:
+Why does...
+
+    from random import seed
+    from random import randint
+    for _ in range(2):
+        print(randint(0, 2))
+
+... only print 2 [newline] 0?
+
+OPS Summary:
+
+"""
+import random
+
+# krewes0 = {2:["Jeff", "Fang"], 7:["7Devo1", "7Devo2"], 8:["8Devo1", "8Devo2"]}
+# print((krewes[2])[random.randint(0,1)])
+
+krewes = {
+           2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY"], 
+           7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+           8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "wanying"]
+         }
+period = [2, 7, 8]
+krewe = krewes[period[random.randint(0,2)]] 
+print(krewe[random.randint(0,len(krewe)-1)])

5b8f28409c6cc868a5dadba2245eefaa87b31b2a
Jeff Chen <cytastorm@gmail.com>
2022-09-30 09:00:21 -0400

    added time spent

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -3,7 +3,7 @@ Jeff Chen
 SoftDev
 K05 - Read, parse, and display contents of txt files containing pds, dev names, and ducky names.
 2022-9-29
-time spent:
+time spent: 45 min
 
 DISCO: 
 Use "with open() as f" with filename, mode to open a file for parsing.

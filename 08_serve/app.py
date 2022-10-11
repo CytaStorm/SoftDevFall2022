253244762d61d5a62a344cdae899e14e803b09da
Jeff <cytastorm@gmail.com>
2022-10-10 20:08:00 -0400

    added code to display the list of jobs on the flask site

--- 08_serve/app.py
+++ 08_serve/app.py
@@ -1,44 +1,59 @@
-"""
-Jeff Chen
-SoftDev
-k07 - flask
-10-2-2022
-time spent:
-30 min
-"""
 # your heading here
 
+# Nicholas Tarsis, Jeff Chen, Vansh Saboo
+# Running Hippos
+# SoftDev
+# k08 - serve
+# 10-6-2022
+# time spent:
+# 0.7
+
 from flask import Flask
-
+import csv
+import random
+#
 app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
+#
 
+def read():
+    jobs = {}
+    with open("./occupations.csv", 'r') as file:
+        csvreader = csv.reader(file)
+        line_count = 0
+        for row in csvreader:
+            if line_count != 0:
+                jobs[row[0]] = float(row[1])
+            line_count = line_count + 1
+        del jobs["Total"]
+    jobs["Other"]= 0.2
+    return jobs
+
+def weighted_select(d):
+    rnum = random.random() * 100
+    running_val_sum = 0
+    line = 0
+    for k in d:
+        if line != 0:
+            running_val_sum += d[k]
+        if running_val_sum >= rnum:
+            return k
+        line += 1
+    return "Other"
+
+def csvToString():
+    result = ""
+    jobsDict = read()
+    for k in jobsDict:
+        result = result + "<br>" + k
+    return result
+
+# final test
 @app.route("/") # Q1: What points of reference do you have for meaning of '/'?
+
 def hello_world():
-    print(__name__) # Q2: Where will this print to? Q3: What will it print?
-    return "No hablo queso!"  # Q4: Will this appear anywhere? How u know?
+#    print(__name__) # Q2: Where will this print to? Q3: What will it print?
 
+    a = read()
+    b = weighted_select(a)
+    return str(b) + "<br>" + csvToString()# Q4: Will this appear anywhere? How u know?
 app.run()  # Q5: Where have you seen similar constructs in other languages?
-
-
-'''
-DISCO:
-Have to use pip install flask
-running the file prints out an ip address used to access the website
-
-QCC:
-0. Haven't seen this anywhere (dunder method?)
-1. Root of file system
-2. Doesn't print anywhere
-3. Nothing
-4. Shows on website
-5. Nowhere? (Java Driver files (methods from other files))
-...
-
-INVESTIGATIVE APPROACH:
-<Your concise summary of how
- you and your team set about
- "illuminating the cave of ignorance" here...>
- 
- Used pip install flask to get flask, but running the file doesn't open anything of interest.
- Opened ip address printed out by running this file in terminal, takes me to a website with "no hablo queso!" printed.
-'''

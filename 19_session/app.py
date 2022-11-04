7101d96a19a5081c28efb487da5c8ad42ead6e7b
Jeff <cytastorm@gmail.com>
2022-11-04 01:19:59 -0400

    retrieved cookies correctly and logout buttom working

--- 19_session/app.py
+++ 19_session/app.py
@@ -2,38 +2,40 @@ from flask import session, Flask, render_template, request
 
 app = Flask(__name__)
 
+
 # Set the secret key to some random bytes. Keep this really secret!
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def index():
     if 'username' in session:
-        return f'Logged in as {session["username"]}'
-    return 'You are not logged in'
+        return render_template('response.html', username=session['username'])
+    return render_template('login.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         session['username'] = request.form['username']
-        return redirect(url_for("127.0.0.1:5000", code=302))
+        print(session['username'])
+        return render_template('response.html', username=session['username'])
     return '''
         <form method="post">
             <p><input type=text name=username>
             <p><input type=submit value=Login>
         </form>
+        <p>
+        Error!
+        <p>
     '''
 
-@app.route('/logout')
+@app.route('/logout', methods=['GET', 'POST'])
 def logout():
     # remove the username from the session if it's there
     session.pop('username', None)
-    return redirect(url_for('/login'))
-
-@app.route('/response')
-def response():
-    return render_template('response.html')
+    return render_template('login.html')
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True 
     app.run()
+

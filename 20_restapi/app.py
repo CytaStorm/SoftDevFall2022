8377cbdade877a2f53be3177a98b622ca1b59f6c
Jeff Chen <cytastorm@gmail.com>
2022-11-22 09:26:11 -0500

    small changes

--- 20_restapi/app.py
+++ 20_restapi/app.py
@@ -2,10 +2,15 @@
 from flask import session, Flask, render_template, request
 
 app = Flask(__name__)
+api = None
+with open('key_nasa.txt', 'r') as f:
+   api = f.read() 
 
 @app.route("/")
 def home():
-    return render_template('main.html')
+    print(api)
+    URL = "https://api.nasa.gov/planetary/apod?api_key=x2f8YnWLKRRkkchhZjqCOsqKGFBAbIzU2IfF0q94"
+    return render_template('main.html', key=URL)
 
 
 if __name__ == "__main__": #false if this file imported as module
